31846e603df9e7c6198f8ad0c8c697a3a8928938
Qualia Touch <qualiatouch@proton.me>
2025-12-31 17:59:40 +0100

    Harmonize screws and add name on widgets

--- src/dmx/DmxOut1Widget.cpp
+++ src/dmx/DmxOut1Widget.cpp
@@ -11,8 +11,8 @@ DmxOut1Widget::DmxOut1Widget(DmxOut1* moduleParam) {
     addChild(createLightCentered<SmallLight<RedLight>>(mm2px(Vec(7.625, 100.0)), module, DmxOut1::BLACKOUT_LIGHT));
     addInput(createInputCentered<PJ301MPort>(mm2px(Vec(7.625, 110.0)), module, DmxOut1::INPUT_BLACKOUT));
 
-    addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-    addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+    addChild(createWidget<ScrewSilver>(Vec(0, 0)));
+    addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH * 2, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
     frameBufferWidget = new FramebufferWidget;
     addChild(frameBufferWidget);
